29799bfc416dae1df87e3545a8ebbac0667c82ac
Aniket <ixaaniketwalanj@gmail.com>
2025-11-05 03:16:42 +0530

    Refactor naive string search function and main

--- naive stiring.c
+++ naive stiring.c
@@ -1,30 +1,30 @@
-
 #include <stdio.h>
 #include <string.h>
 
-void naiveString (char text[],char pattern[]){
+void naive_string(char text[],char pat[]) {
     int n = strlen(text);
-    int m = strlen(pattern);
-    int i , j;
-
-    for(i=0;i<=n-m;i++){
-        for(j=0;j<m;j++){
-            if(text[i+j] != pattern[j])
-                break;
-        }
-        if(j==m){
-            printf("Pattern found at index : %d",i);
-        }
-    }
+	int m = strlen(pat);
+	int i, j;
+	for(i=0; i<=n-m; i++) {
+		for(j=0; j<m; j++) {
+			if(text[i+j]!=pat[j]) {
+				break;
+			}
+		}
+		if(j==m) {
+			printf("Pattern found at index %d",i);
+		}
+	}
 }
+
 int main(){
-    char text[100],pattern[100];
-    printf("Enter the text: ");
-    fgets(text, sizeof(text), stdin);
-
-    printf("Enter the pattern: ");
-    fgets(pattern, sizeof(pattern), stdin);
-
-    naiveString(text,pattern);
+    char text[100],pat[100];
+    printf("Enter the text:");
+    gets(text);
+    
+    printf("Enter the patern to be checked: ");
+    gets(pat);
+    
+    naive_string(text,pat);
     return 0;
 }
