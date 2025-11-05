9314d4cc79c8fb3330eb734c4ef6cdb17f3058c2
Aniket <ixaaniketwalanj@gmail.com>
2025-11-05 16:41:36 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,49 +1,53 @@
 #include <stdio.h>
 
-void mergesort(int arr[],int lb,int ub){
+void merge(int arr[],int lb, int mid,int ub){
+    int temp_size = (ub-lb)+1;
+    int i = lb;
+    int j = mid+1;
+    int k = 0;
+    int temp[temp_size];
+    while(i<=mid && j<=ub){
+        if(arr[i]<=arr[j]){
+            temp[k] = arr[i];
+            i++,k++;
+        }
+        else{
+            temp[k]=arr[j];
+            j++,k++;
+        }
+    }
+    while(j<=ub){
+        temp[k] = arr[j];
+        j++,k++;
+    }
+    while(i<=mid){
+        temp[k]=arr[i];
+        i++,k++;
+    }
+    for(i=0;i<temp_size;i++){
+        arr[lb+i]=temp[i];
+    }
+}
+
+void mergesort(int arr[],int lb, int ub){
     if(lb<ub){
-        int mid = (lb + ub)/2;
+        int mid = (lb+ub)/2;
         mergesort(arr,lb,mid);
         mergesort(arr,mid+1,ub);
         merge(arr,lb,mid,ub);
     }
 }
 
-void merge(int arr[],int lb, int mid, int ub){
-    int b_size = ub-lb + 1;
-    int i = lb;
-    int j = mid + 1;
-    int k =0;
-    int b[b_size];
-    while(i<=mid && j<=ub){
-        if(arr[i]<=arr[j]){
-            b[k] = arr[i];
-            i++,k++;
-        }
-        else{
-            b[k] = arr[j];
-            j++,k++;
-        }
-    }
-    while(j<=ub){
-        b[k] = arr[j];
-        j++,k++;
-    }
-    while(i<=mid){
-        b[k] = arr[i];
-        i++,k++;
-    }
-    for(i = 0;i<b_size;i++){
-        arr[lb+i] = b[i];
-    }
-}
-
 int main(){
-    int arr[] = {22,4,56,5,78,9};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[20],n;
+    printf("Enter no. elements: ");
+    scanf("%d",&n);
+    printf("Enter elements: ");
+    for(int i = 0 ; i<n;i++){
+        scanf("%d",&arr[i]);
+    }
     mergesort(arr,0,n-1);
-    printf("Sorted array: \n");
-    for(int x = 0;x<n;x++){
-        printf("%d, ",arr[x]);
+    for(int x = 0 ; x<n;x++){
+        printf("%d ",arr[x]);
     }
 }
