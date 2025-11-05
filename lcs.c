0bce69ae6cf740ed8a7cf62e31a5c32889e42813
Aniket <ixaaniketwalanj@gmail.com>
2025-11-05 11:30:44 +0530

    Refactor LCS program for user input and memory fix
    Updated the LCS implementation to allow user input for strings and fixed memory allocation issues.

--- lcs.c
+++ lcs.c
@@ -1,46 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-int max(int a, int b) {
-    return (a > b) ? a : b;
+int max(int a , int b){
+    return (a>b) ? a:b;
 }
 
-//x and y are two pointers to the starting of memory of string arrays and m and n are their respective lenghths
-void lcs_length(char *X, char *Y, int m , int n , int **dp){
-    for(int i=0;i<=m;i++){
-        for(int j=0;j<=n;j++){
-            if(i==0 || j==0){
+void lcs_length(char *X,char *Y,int m , int n , int **dp){
+    for(int i = 0 ; i<=m ; i++){
+        for(int j = 0 ; j<=n;j++){
+            if(i==0||j==0){
                 dp[i][j]=0;
             }
             else if(X[i-1]==Y[j-1]){
-                dp[i][j]=dp[i-1][j-1]+1;
+                dp[i][j] =  dp[i-1][j-1]+1;
             }
             else{
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
             }
         }
     }
-    printf("length of LCS = %d\n",dp[m][n]);
+    printf("Length of LCS: %d",dp[m][n]);
 }
 
-void print_lcs(char *X,char *Y, int m,int n, int **dp){
-    int length = dp[m][n];
-    if(length==0){
-        printf("Dosen't Exist");
-        return;
-    }
-    char *lcs=(char*)malloc((length+1)*sizeof(char));
-    if(lcs==NULL) return;
-    lcs[length] = '\0';
-    int i = m, j = n;
-    int index = length - 1;
+void print_lcs(char *X,char *Y,int m , int n , int **dp){
+    int length=dp[m][n];
+    char *lcs = (char*)malloc((length+1)*sizeof(char));
+    lcs[length] =  '\0';
+    int i=m,j=n;
+    int index=length-1;
     while(i>0 && j>0){
         if(X[i-1]==Y[j-1]){
-            lcs[index] = X[i-1];
-            i--;
-            j--;
-            index--;
+            lcs[index]=X[i-1];
+            i--,j--,index--;
         }
         else if(dp[i-1][j]>dp[i][j-1]){
             i--;
@@ -49,28 +41,32 @@ void print_lcs(char *X,char *Y, int m,int n, int **dp){
             j--;
         }
     }
-    printf("LCS: %s\n", lcs);
+    printf("\nLCS: %s",lcs);
     free(lcs);
+    
 }
 
-void solve_lcs(char* X, char* Y){
-    int m = strlen(X);
-    int n = strlen(Y);
-
+void lcs_solver(char *X,char *Y,int m , int n ){
     int **dp = (int **)malloc((m+1)*sizeof(int *));
-    if(dp==NULL) return 1;
-    for(int i = 0;i<=m;i++){
-        dp[i] = (int **)malloc((n+1)*sizeof(int *));
+    
+    
+    for(int a = 0;a<=m;a++){
+        dp[a]=(int *)malloc((n+1)*sizeof(int));
     }
     lcs_length(X,Y,m,n,dp);
     print_lcs(X,Y,m,n,dp);
 }
 
 int main() {
-    char X[] = "AGGTAB";
-    char Y[] = "GXTXAYB";
+    char X[100], Y[100]; 
+
+    printf("Enter first string: ");
+    scanf("%s", X);
+
+    printf("Enter second string: ");
+    scanf("%s", Y);
     int m = strlen(X);
     int n = strlen(Y);
-    solve_lcs(X,Y);
+    lcs_solver(X,Y,m,n);
 
 }
