7f751bcc9cfb91549ae5d9ed1efcd7d94f800d09
Aniket <ixaaniketwalanj@gmail.com>
2025-11-05 10:41:08 +0530

    Update 01 knapsack.c

--- 01 knapsack.c
+++ 01 knapsack.c
@@ -1,53 +1,63 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-// Function to find the maximum of two integers
+// find max of two numbers
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-// Function to solve the 0/1 Knapsack problem using Dynamic Programming
+// knapsack function (does everything)
 int knapsack_dp(int W, int weights[], int values[], int n) {
-    // T[i][w] will store the maximum value that can be obtained
-    int T[n + 1][W + 1];
-    int i, w;    //rows,columns
+    int dp[n + 1][W + 1];
 
-    for (i = 0; i <= n; i++) {
-        for (w = 0; w <= W; w++) {
-
-            if (i == 0 || w == 0) {
-                T[i][w] = 0;
-            }
-
-            else if (weights[i - 1] <= w) {
-                T[i][w] = max(values[i - 1] + T[i - 1][w - weights[i - 1]], T[i - 1][w]);
-            }
-            else {
-                T[i][w] = T[i - 1][w];
-            }
+    // fill dp table
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (weights[i - 1] <= w)
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
         }
     }
 
-    // The result is the maximum value in the last cell of the table
-    return T[n][W];
+    // print max value
+    printf("\nMaximum value: %d\n", dp[n][W]);
+
+    // find which items were chosen
+    int w = W;
+    printf("Items chosen (1-based index): ");
+    for (int i = n; i > 0 && w > 0; i--) {
+        if (dp[i][w] != dp[i - 1][w]) {  // means item was included
+            printf("%d ", i);
+            w -= weights[i - 1];
+        }
+    }
+    printf("\n");
+
+    return dp[n][W];
 }
 
-// --- Main Driver Code ---
+// main function
 int main() {
+    int n, W;
 
-    printf("0/1 Knapsack Problem using Dynamic Programming\n");
-    printf("Items: {Value, Weight}\n");
-    for(int i = 0; i < n; i++) {
-        printf("  Item %d: {%d, %d}\n", i + 1, values[i], weights[i]);
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    int values[n], weights[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter value for item %d: ", i + 1);
+        scanf("%d", &values[i]);
+        printf("Enter weight for item %d: ", i + 1);
+        scanf("%d", &weights[i]);
     }
-    printf("Knapsack Capacity (W): %d\n\n", W);
 
-    int max_val = knapsack_dp(W, weights, values, n);
+    printf("Enter knapsack capacity: ");
+    scanf("%d", &W);
 
-    printf("Maximum value that can be put in the knapsack: %d\n", max_val);
+    knapsack_dp(W, weights, values, n);
 
     return 0;
 }
-
